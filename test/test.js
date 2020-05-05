4d2cbd3c5165a93fff85ecd8a34d59335b255d2d
Your Name <you@example.com>
2020-05-05 16:35:59 +0200

    change title

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-  it("respond with Hello World via Github Actions", (done) => {
-    request(app).get("/").expect("Hello World via Github Actions", done);
+  it("respond with Hello Capgemini via Github Actions", (done) => {
+    request(app).get("/").expect("Hello Capgemini via Github Actions", done);
   })
 });
